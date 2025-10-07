65aa562a9408ba5e5dc0ec942b0f78fcb2ecfeb6
salmasobhi <salmasobhi951@gmail.com>
2025-10-07 10:28:38 +0300

    Fix: Implement 'useNavigation' fix and finalize EAS Build setup

--- navigation/Rootstack.tsx
+++ navigation/Rootstack.tsx
@@ -1,32 +1,3 @@
-// import HomeScreen from "@/screens/HomeScreen";
-// import LoginScreenRQ from "@/screens/LoginScreenRQ";
-// import AsyncStorage from "@react-native-async-storage/async-storage";
-// import { useNavigation } from "@react-navigation/native";
-// import { createNativeStackNavigator, NativeStackNavigationProp } from "@react-navigation/native-stack";
-// import React, { useEffect, useState } from "react";
-// export type RootStackParamList = {
-//   LoginScreenRQ: undefined;
-//   HomeScreen: undefined;
-// };
-// export type RootStackNavigationProp = NativeStackNavigationProp<
-//   RootStackParamList,
-//   keyof RootStackParamList
-// >;
-
-// const Stack = createNativeStackNavigator<RootStackParamList>();
-
-// const Rootstack = () => {
-//   return (
-//     <Stack.Navigator initialRouteName="LoginScreenRQ">
-//       <Stack.Screen name="LoginScreenRQ" component={LoginScreenRQ} />
-//       <Stack.Screen name="HomeScreen" component={HomeScreen} />
-//     </Stack.Navigator>
-//   );
-// };
-
-// export default Rootstack;
-
-
 
 import HomeScreen from "@/screens/HomeScreen";
 import LoginScreenRQ from "@/screens/LoginScreenRQ";
@@ -42,7 +13,7 @@ export type RootStackParamList = {
 
 //This TypeScript type defines the navigation prop based on the stack you have created. (NativeStackNavigationProp)
 export type RootStackNavigationProp = 
-  import("@react-navigation/native-stack").NativeStackNavigationProp<RootStackParamList>;
+import("@react-navigation/native-stack").NativeStackNavigationProp<RootStackParamList>;
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -68,7 +39,6 @@ const Rootstack: React.FC = () => {
       </View>
     );
   }
-
   return (
     <Stack.Navigator initialRouteName={initialRoute} screenOptions={{ headerShown: false }}>
       <Stack.Screen name="LoginScreenRQ" component={LoginScreenRQ} />
@@ -76,5 +46,4 @@ const Rootstack: React.FC = () => {
     </Stack.Navigator>
   );
 };
-
 export default Rootstack;
